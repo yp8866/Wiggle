65a8ccf4008c366887ca44ad36e00dbbc10ee6cc
yp8866 <122187543+yp8866@users.noreply.github.com>
2023-11-17 23:34:14 +0530

    updated

---
+++ temp data/items.js
@@ -0,0 +1,814 @@
+var restraname=['Spice Junction','Curry House','Tandoori Delights','Saffron Palace','Masala Mantra','Biryani Paradise','Naan Nation','Dosa Dhabha','Mughal Mahal','Punjabi Dhabha','Roll Corner','Samosa Street','Samosa Street-2','Chai Chart'];
+
+var itemarray= [
+    
+    // spice junction
+    {
+
+    title: 'Turkey-Burger',
+    price: 80,
+    image: '/images/restaurants/0.jpg'
+},
+{
+    title: 'Coffee',
+    price: 100,
+    image: '/images/restaurants/1.jpg'
+},
+{
+    title: 'Cold-Coffee',
+    price: 200,
+    image: '/images/restaurants/2.jpg'
+},
+{
+    title: 'Chicken-Biryani',
+    price: 300,
+    image: '/images/restaurants/3.jpg'
+},
+{
+    title: 'Masala-Dosa',
+    price: 400,
+    image: '/images/restaurants/4.jpg'
+},
+{
+    title: 'Cabbage-Momos',
+    price: 500,
+    image: '/images/restaurants/5.jpg'
+},
+{
+    title: 'Chicken-Roast',
+    price: 600,
+    image: '/images/restaurants/6.jpg'
+},
+{
+    title: 'Chowmin',
+    price: 700,
+    image: '/images/restaurants/7.jpg'
+},
+{
+    title: 'Idli-Rice',
+    price: 800,
+    image: '/images/restaurants/8.jpg'
+},
+{
+    title: 'Vegetable-Curry',
+    price: 900,
+    image: '/images/restaurants/9.jpg'
+},
+
+
+// curry house
+{
+    title: 'Mixjuice',
+    price: 80,
+    image: '/images/restaurants/10.jpg'
+},
+{
+    title: 'Special-Coffee',
+    price: 100,
+    image: '/images/restaurants/11.jpg'
+},
+{
+    title: 'Cold-Coffee',
+    price: 200,
+    image: '/images/restaurants/12.jpg'
+},
+{
+    title: 'Veg-Burger',
+    price: 300,
+    image: '/images/restaurants/13.jpg'
+},
+{
+    title: 'Ice-Cream',
+    price: 400,
+    image: '/images/restaurants/14.jpg'
+},
+{
+    title: 'Shahi-Kachori',
+    price: 500,
+    image: '/images/restaurants/15.jpg'
+},
+{
+    title: 'Mix-Icecreams',
+    price: 600,
+    image: '/images/restaurants/16.jpg'
+},
+{
+    title: 'Special-Sweets',
+    price: 700,
+    image: '/images/restaurants/17.jpg'
+},
+{
+    title: 'Veg-Noodles',
+    price: 800,
+    image: '/images/restaurants/18.jpg'
+},
+{
+    title: 'Rolls',
+    price: 900,
+    image: '/images/restaurants/19.jpg'
+},
+
+// Tandoori Delights
+{
+    title: 'Sandwiches',
+    price: 80,
+    image: '/images/restaurants/20.jpg'
+},
+{
+    title: 'Cake',
+    price: 100,
+    image: '/images/restaurants/21.jpg'
+},
+{
+    title: 'Cup-Cake',
+    price: 200,
+    image: '/images/restaurants/22.jpg'
+},
+{
+    title: 'Special-Roti',
+    price: 300,
+    image: '/images/restaurants/23.jpg'
+},
+{
+    title: 'Birthday-Cake',
+    price: 400,
+    image: '/images/restaurants/24.jpg'
+},
+{
+    title: 'Special-Cake',
+    price: 500,
+    image: '/images/restaurants/25.jpg'
+},
+{
+    title: 'Coffee',
+    price: 600,
+    image: '/images/restaurants/26.jpg'
+},
+{
+    title: 'Cup-Coffee',
+    price: 700,
+    image: '/images/restaurants/27.jpg'
+},
+{
+    title: 'Egg-Biryani',
+    price: 800,
+    image: '/images/restaurants/28.jpg'
+},
+{
+    title: 'Milk Cake',
+    price: 900,
+    image: '/images/restaurants/29.jpg'
+},
+
+// Saffron Palace
+{
+    title: 'Chicken-Biryani',
+    price: 80,
+    image: '/images/restaurants/30.jpg'
+},
+{
+    title: 'Chicken-Curry',
+    price: 100,
+    image: '/images/restaurants/31.jpg'
+},
+{
+    title: 'Chicken-Manchurian',
+    price: 200,
+    image: '/images/restaurants/32.jpg'
+},
+{
+    title: 'Handi-Chicken',
+    price: 300,
+    image: '/images/restaurants/33.jpg'
+},
+{
+    title: 'Cake',
+    price: 400,
+    image: '/images/restaurants/34.jpg'
+},
+{
+    title: 'Plain-Dosa',
+    price: 500,
+    image: '/images/restaurants/35.jpg'
+},
+{
+    title: 'Drinks',
+    price: 600,
+    image: '/images/restaurants/36.jpg'
+},
+{
+    title: 'Veg-Pizza',
+    price: 700,
+    image: '/images/restaurants/37.jpg'
+},
+{
+    title: 'Chicken-Pyaza',
+    price: 800,
+    image: '/images/restaurants/38.jpg'
+},
+{
+    title: 'Chicken-Masala',
+    price: 900,
+    image: '/images/restaurants/39.jpg'
+},
+
+// masala mantra
+{
+    title: 'Coffe',
+    price: 80,
+    image: '/images/restaurants/40.jpg'
+},
+{
+    title: 'Cold-Coffee',
+    price: 100,
+    image: '/images/restaurants/41.jpg'
+},
+{
+    title: 'Spring-Rolls',
+    price: 200,
+    image: '/images/restaurants/42.jpg'
+},
+{
+    title: 'Chilli-Chicken',
+    price: 300,
+    image: '/images/restaurants/43.jpg'
+},
+{
+    title: 'Goan-Biryani',
+    price: 400,
+    image: '/images/restaurants/44.jpg'
+},
+{
+    title: 'Matar-Paneer',
+    price: 500,
+    image: '/images/restaurants/45.jpg'
+},
+{
+    title: 'Shake',
+    price: 600,
+    image: '/images/restaurants/46.jpg'
+},
+{
+    title: 'Szechuan-Chicken',
+    price: 700,
+    image: '/images/restaurants/47.jpg'
+},
+{
+    title: 'Veg-Fried-Rice',
+    price: 800,
+    image: '/images/restaurants/48.jpg'
+},
+{
+    title: 'Vegetable-Masala',
+    price: 900,
+    image: '/images/restaurants/49.jpg'
+},
+
+// biryani paradise
+{
+    title: 'Coffee',
+    price: 80,
+    image: '/images/restaurants/50.jpg'
+},
+{
+    title: 'Cold-Coffee',
+    price: 100,
+    image: '/images/restaurants/51.jpg'
+},
+{
+    title: 'Kamrupi-Biryani',
+    price: 200,
+    image: '/images/restaurants/52.jpg'
+},
+{
+    title: 'Rice-Bryani',
+    price: 300,
+    image: '/images/restaurants/53.jpg'
+},
+{
+    title: 'Momos',
+    price: 400,
+    image: '/images/restaurants/54.jpg'
+},
+{
+    title: 'Memoni-Biryani',
+    price: 500,
+    image: '/images/restaurants/55.jpg'
+},
+{
+    title: 'Mughlai-Biryani',
+    price: 600,
+    image: '/images/restaurants/56.jpg'
+},
+{
+    title: 'Kashmiri-Pulao',
+    price: 700,
+    image: '/images/restaurants/57.jpg'
+},
+{
+    title: 'Masala-Dosa',
+    price: 800,
+    image: '/images/restaurants/58.jpg'
+},
+{
+    title: 'Murgh-Musallam',
+    price: 900,
+    image: '/images/restaurants/59.jpg'
+},
+
+// naan nation
+{
+    title: 'Coffee',
+    price: 80,
+    image: '/images/restaurants/60.jpg'
+},
+{
+    title: 'Cold-Coffee',
+    price: 100,
+    image: '/images/restaurants/61.jpg'
+},
+{
+    title: 'Mysore-Bonda',
+    price: 200,
+    image: '/images/restaurants/62.jpg'
+},
+{
+    title: 'Korma-Vegetable',
+    price: 300,
+    image: '/images/restaurants/63.jpg'
+},
+{
+    title: 'Onion-Pizza',
+    price: 400,
+    image: '/images/restaurants/64.jpg'
+},
+{
+    title: 'Palak-Paneer',
+    price: 500,
+    image: '/images/restaurants/65.jpg'
+},
+{
+    title: 'Paneer-Butter',
+    price: 600,
+    image: '/images/restaurants/66.jpg'
+},
+{
+    title: 'Paneer-Pyaza',
+    price: 700,
+    image: '/images/restaurants/67.jpg'
+},
+{
+    title: 'Ras-Gulla',
+    price: 800,
+    image: '/images/restaurants/68.jpg'
+},
+{
+    title: 'Rava-Uttapam',
+    price: 900,
+    image: '/images/restaurants/69.jpg'
+},
+
+// dosa dhabha
+{
+    title: 'Veg-Jalfrezi',
+    price: 80,
+    image: '/images/restaurants/70.jpg'
+},
+{
+    title: 'Shakes',
+    price: 100,
+    image: '/images/restaurants/71.jpg'
+},
+{
+    title: 'Mix-Shakes',
+    price: 200,
+    image: '/images/restaurants/72.jpg'
+},
+{
+    title: 'Shahi-Paneer',
+    price: 300,
+    image: '/images/restaurants/73.jpg'
+},
+{
+    title: 'Pastries',
+    price: 400,
+    image: '/images/restaurants/74.jpg'
+},
+{
+    title: 'Paneer-Hyderabadi',
+    price: 500,
+    image: '/images/restaurants/75.jpg'
+},
+{
+    title: 'Paneer-Lababdar',
+    price: 600,
+    image: '/images/restaurants/76.jpg'
+},
+{
+    title: 'Sambhar-Vada',
+    price: 700,
+    image: '/images/restaurants/77.jpg'
+},
+{
+    title: 'Vegetable-Biryani',
+    price: 800,
+    image: '/images/restaurants/78.jpg'
+},
+{
+    title: 'Vegetable-Kolhapuri',
+    price: 900,
+    image: '/images/restaurants/79.jpg'
+},
+
+// mughal mahal
+{
+    title: 'Bagel-Pizza',
+    price: 80,
+    image: '/images/restaurants/80.jpg'
+},
+{
+    title: 'Caviar-Pizza',
+    price: 100,
+    image: '/images/restaurants/81.jpg'
+},
+{
+    title: 'Marrow-Pizza',
+    price: 200,
+    image: '/images/restaurants/82.jpg'
+},
+{
+    title: 'Breakfast-Pizza',
+    price: 300,
+    image: '/images/restaurants/83.jpg'
+},
+{
+    title: 'Bresaola-Pizza',
+    price: 400,
+    image: '/images/restaurants/84.jpg'
+},
+{
+    title: 'Californian-Pizza',
+    price: 500,
+    image: '/images/restaurants/85.jpg'
+},
+{
+    title: 'Calzone-Pizza',
+    price: 600,
+    image: '/images/restaurants/86.jpg'
+},
+{
+    title: 'Chicago-Pizza',
+    price: 700,
+    image: '/images/restaurants/87.jpg'
+},
+{
+    title: 'Chicken-Masala',
+    price: 800,
+    image: '/images/restaurants/88.jpg'
+},
+{
+    title: 'Chorizo-Pizza',
+    price: 900,
+    image: '/images/restaurants/89.jpg'
+},
+
+// punjabi dhabha
+{
+    title: 'Dessert-Pizza',
+    price: 80,
+    image: '/images/restaurants/90.jpg'
+},
+{
+    title: 'Detroit-Pizza',
+    price: 100,
+    image: '/images/restaurants/91.jpg'
+},
+{
+    title: 'French-Bread-Pizza',
+    price: 200,
+    image: '/images/restaurants/92.jpg'
+},
+{
+    title: 'Marinara-Pizza',
+    price: 300,
+    image: '/images/restaurants/93.jpg'
+},
+{
+    title: 'Meatball-Pizza',
+    price: 400,
+    image: '/images/restaurants/94.jpg'
+},
+{
+    title: 'Mexican-Pizza',
+    price: 500,
+    image: '/images/restaurants/95.jpg'
+},
+{
+    title: 'Neapolitan-Pizza',
+    price: 600,
+    image: '/images/restaurants/96.jpg'
+},
+{
+    title: 'New-York -Pizza',
+    price: 700,
+    image: '/images/restaurants/97.jpg'
+},
+{
+    title: 'Nova-Garlic-Pizza',
+    price: 800,
+    image: '/images/restaurants/98.jpg'
+},
+{
+    title: 'Pizza-Margherita',
+    price: 900,
+    image: '/images/restaurants/99.jpg'
+},
+
+
+// roll corner
+{
+    title: 'Portuguesa',
+    price: 80,
+    image: '/images/restaurants/100.jpg'
+},
+{
+    title: 'Provencale',
+    price: 100,
+    image: '/images/restaurants/101.jpg'
+},
+{
+    title: 'Roman-Pizza',
+    price: 200,
+    image: '/images/restaurants/102.jpg'
+},
+{
+    title: 'Russian-Pizza',
+    price: 300,
+    image: '/images/restaurants/103.jpg'
+},
+{
+    title: 'Sicilian-Pizza',
+    price: 400,
+    image: '/images/restaurants/104.jpg'
+},
+{
+    title: 'Louis-Pizza',
+    price: 500,
+    image: '/images/restaurants/105.jpg'
+},
+{
+    title: 'Stromboli',
+    price: 600,
+    image: '/images/restaurants/106.jpg'
+},
+{
+    title: 'Veggie-Pizza',
+    price: 700,
+    image: '/images/restaurants/107.jpg'
+},
+{
+    title: 'Falafel-Burgers',
+    price: 800,
+    image: '/images/restaurants/108.jpg'
+},
+{
+    title: 'Delish-Burger',
+    price: 900,
+    image: '/images/restaurants/109.jpg'
+},
+
+// samosa street
+{
+    title: 'Burger-Anatomy',
+    price: 80,
+    image: '/images/restaurants/110.jpg'
+},
+{
+    title: 'Chicken-Burger',
+    price: 100,
+    image: '/images/restaurants/111.jpg'
+},
+{
+    title: 'Fryer-Burger',
+    price: 200,
+    image: '/images/restaurants/112.jpg'
+},
+{
+    title: 'Krabby-Patty',
+    price: 300,
+    image: '/images/restaurants/113.jpg'
+},
+{
+    title: 'Ramen-Burgers',
+    price: 400,
+    image: '/images/restaurants/114.jpg'
+},
+{
+    title: 'Veggie-Burgers',
+    price: 500,
+    image: '/images/restaurants/115.jpg'
+},
+{
+    title: 'Iceburgers',
+    price: 600,
+    image: '/images/restaurants/116.jpg'
+},
+{
+    title: 'Cake-Burgers',
+    price: 700,
+    image: '/images/restaurants/117.jpg'
+},
+{
+    title: 'Burger-Delish',
+    price: 800,
+    image: '/images/restaurants/118.jpg'
+},
+{
+    title: 'Salmon-Burger',
+    price: 900,
+    image: '/images/restaurants/119.jpg'
+},
+
+// samosa street
+{
+    title: 'Pineapple-Burger',
+    price: 80,
+    image: '/images/restaurants/120.jpg'
+},
+{
+    title: 'Big-Mac-Burger',
+    price: 100,
+    image: '/images/restaurants/121.jpg'
+},
+{
+    title: 'Delish-Kings',
+    price: 200,
+    image: '/images/restaurants/122.jpg'
+},
+{
+    title: 'Shack-Burger',
+    price: 300,
+    image: '/images/restaurants/123.jpg'
+},
+{
+    title: 'Cheeseburger',
+    price: 400,
+    image: '/images/restaurants/124.jpg'
+},
+{
+    title: 'Chili-Cheeseburger',
+    price: 500,
+    image: '/images/restaurants/125.jpg'
+},
+{
+    title: 'Turkey-Burger',
+    price: 600,
+    image: '/images/restaurants/126.jpg'
+},
+{
+    title: 'Greek-Burgers',
+    price: 700,
+    image: '/images/restaurants/127.jpg'
+},
+{
+    title: 'Stock-Burgers',
+    price: 800,
+    image: '/images/restaurants/128.jpg'
+},
+{
+    title: 'Popper-Burgers',
+    price: 900,
+    image: '/images/restaurants/129.jpg'
+},
+
+// chai chaat
+{
+    title: 'Baked-Burger',
+    price: 80,
+    image: '/images/restaurants/130.jpg'
+},
+{
+    title: 'Mushroom-Burger',
+    price: 100,
+    image: '/images/restaurants/131.jpg'
+},
+{
+    title: 'Katsu-Burger',
+    price: 200,
+    image: '/images/restaurants/132.jpg'
+},
+{
+    title: 'Shroomami-Burger',
+    price: 300,
+    image: '/images/restaurants/133.jpg'
+},
+{
+    title: 'Bison-Burger',
+    price: 400,
+    image: '/images/restaurants/134.jpg'
+},
+{
+    title: 'Bean-Burgers',
+    price: 500,
+    image: '/images/restaurants/135.jpg'
+},
+{
+    title: 'Chicken-Burger',
+    price: 600,
+    image: '/images/restaurants/136.jpg'
+},
+{
+    title: 'Lamb-Burger',
+    price: 700,
+    image: '/images/restaurants/137.jpg'
+},
+{
+    title: 'Pizza-Burgers',
+    price: 800,
+    image: '/images/restaurants/138.jpg'
+},
+{
+    title: 'Vegan-Burger',
+    price: 900,
+    image: '/images/restaurants/139.jpg'
+},
+{
+    title: 'Smash-Burgers',
+    price: 80,
+    image: '/images/restaurants/140.jpg'
+},
+{
+    title: 'Square-Lena',
+    price: 100,
+    image: '/images/restaurants/141.jpg'
+}
+]
+
+
+var text="";
+
+
+for(var cl=0;cl<14;cl++){
+    let count=0;
+    for(let k=0;k<10;k++){
+        let id=(cl*10)+k;
+    
+          let title=itemarray[id].title;
+          let price=itemarray[id].price;
+          let image=itemarray[id].image;
+      
+      if(count%10 <4){
+          let othrpric=price+200;
+          // container.insertAdjacentHTML('afterend',
+          text+="\n"+
+          `
+          <div class="dishitem">
+                  <img src='${image}' alt="">
+                  <h3>${title}</h3>
+                  <h5 class="offer"><b>Offer Price:&nbsp;</b>&#8377;${price}/-</h5>
+                  <h5><b>Normal Price:&nbsp;</b><strike>&#8377;${othrpric}/-</strike></h5>
+                  <h5><b>Restaurant Name:&nbsp;</b>${restraname[cl]}</h5>
+                  <button class="add-to-cart" onclick="addtocart('${id}')">Add to Cart</button>
+              </div>
+  
+          `
+  
+  
+          // )
+      }
+      else{
+  
+        // container.insertAdjacentHTML('afterend',
+        text+="\n"+
+        `
+        <div class="dishitem">
+                  <img src='${image}' alt="">
+                  <h3>${title}</h3>
+                  <h5><b>Normal Price:&nbsp;</b>&#8377;${price}/-</h5>
+                  <h5><b>Restaurant Name:&nbsp;</b>${restraname[cl]}</h5>
+                  <button class="add-to-cart" onclick="addtocart('${id}')">Add to Cart</button>
+              </div>
+        
+        `
+  
+  
+        
+        // )
+  
+      }
+
+      
+      count++;
+  
+  
+  
+  
+    }
+
+    text+="\n\n\n\n\n";
+  
+
+  
+}
+
+console.log(text);
